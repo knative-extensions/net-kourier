65fc17e5b936f53b1cb9a896d446b85ac49fb1db
David Ortiz <z.david.ortiz@gmail.com>
2019-11-20 14:38:03 +0100

    config: add Namespace()

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,10 +1,44 @@
 package config
 
+import (
+	"os"
+	"sync"
+
+	log "github.com/sirupsen/logrus"
+)
+
 const (
-	HttpPortExternal      = uint32(8080)
-	HttpPortInternal      = uint32(8081)
-	HttpsPortExternal     = uint32(8443)
+	HttpPortExternal  = uint32(8080)
+	HttpPortInternal  = uint32(8081)
+	HttpsPortExternal = uint32(8443)
+
 	InternalKourierHeader = "kourier-snapshot-id"
 	InternalKourierDomain = "internalkourier"
 	InternalKourierPath   = "/__internalkouriersnapshot"
+
+	KourierNamespaceEnv     = "KOURIER_NAMESPACE"
+	KourierDefaultNamespace = "knative-serving"
 )
+
+var kourierNamespace string
+var once sync.Once
+
+func Namespace() string {
+	once.Do(
+		func() {
+			kourierNamespace = os.Getenv(KourierNamespaceEnv)
+
+			if kourierNamespace == "" {
+				log.Infof(
+					"Env %s empty, using default: %s",
+					KourierNamespaceEnv,
+					KourierDefaultNamespace,
+				)
+
+				kourierNamespace = KourierDefaultNamespace
+			}
+		},
+	)
+
+	return kourierNamespace
+}
