54cb65e127ec40380c202801198a869686c7a3d7
Markus Thömmes <markusthoemmes@me.com>
2020-05-13 11:09:57 +0200

    Fix module usage in presubmit tests and update scripts. (#66)
    * Fix module usage in presubmit tests and update scripts.
    
    * Add missing image to tools.

--- hack/tools.go
+++ hack/tools.go
@@ -8,6 +8,7 @@ import (
 	_ "knative.dev/serving/test/conformance/ingress"
 	_ "knative.dev/serving/test/test_images/flaky"
 	_ "knative.dev/serving/test/test_images/grpc-ping"
+	_ "knative.dev/serving/test/test_images/helloworld"
 	_ "knative.dev/serving/test/test_images/httpproxy"
 	_ "knative.dev/serving/test/test_images/runtime"
 	_ "knative.dev/serving/test/test_images/timeout"
