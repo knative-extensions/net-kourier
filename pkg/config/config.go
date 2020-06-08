100a246a670179dee32b98fd0d40ded06490ba73
David Ortiz <z.david.ortiz@gmail.com>
2020-06-08 16:01:19 +0200

    config: remove unused InternalKourierPath (#94)

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -29,7 +29,6 @@ const (
 	HTTPSPortExternal = uint32(8443)
 
 	InternalKourierDomain    = "internalkourier"
-	InternalKourierPath      = "/__internalkouriersnapshot"
 	ExtAuthzHostEnv          = "KOURIER_EXTAUTHZ_HOST"
 	ExtAuthzFailureModeEnv   = "KOURIER_EXTAUTHZ_FAILUREMODEALLOW"
 	ExtAuthzMaxRequestsBytes = "KOURIER_EXTAUTHZ_MAXREQUESTBYTES"
