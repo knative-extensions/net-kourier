40e62ca58d7c7cf5e815838a030b48676ae2c525
Matt Moore <mattmoor@vmware.com>
2020-06-22 07:20:26 -0700

    [master] Auto-update dependencies (#107)
    Produced via:
      `./hack/update-deps.sh --upgrade && ./hack/update-codegen.sh`
    /assign davidor markusthoemmes
    /cc davidor markusthoemmes

--- vendor/k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1/defaults.go
+++ vendor/k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1/defaults.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 The Kubernetes Authors.
+Copyright 2019 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package v1beta1
+package v1
 
 import (
 	"strings"
@@ -40,37 +40,16 @@ func SetDefaults_CustomResourceDefinition(obj *CustomResourceDefinition) {
 }
 
 func SetDefaults_CustomResourceDefinitionSpec(obj *CustomResourceDefinitionSpec) {
-	if len(obj.Scope) == 0 {
-		obj.Scope = NamespaceScoped
-	}
 	if len(obj.Names.Singular) == 0 {
 		obj.Names.Singular = strings.ToLower(obj.Names.Kind)
 	}
 	if len(obj.Names.ListKind) == 0 && len(obj.Names.Kind) > 0 {
 		obj.Names.ListKind = obj.Names.Kind + "List"
 	}
-	// If there is no list of versions, create on using deprecated Version field.
-	if len(obj.Versions) == 0 && len(obj.Version) != 0 {
-		obj.Versions = []CustomResourceDefinitionVersion{{
-			Name:    obj.Version,
-			Storage: true,
-			Served:  true,
-		}}
-	}
-	// For backward compatibility set the version field to the first item in versions list.
-	if len(obj.Version) == 0 && len(obj.Versions) != 0 {
-		obj.Version = obj.Versions[0].Name
-	}
 	if obj.Conversion == nil {
 		obj.Conversion = &CustomResourceConversion{
 			Strategy: NoneConverter,
 		}
-	}
-	if obj.Conversion.Strategy == WebhookConverter && len(obj.Conversion.ConversionReviewVersions) == 0 {
-		obj.Conversion.ConversionReviewVersions = []string{SchemeGroupVersion.Version}
-	}
-	if obj.PreserveUnknownFields == nil {
-		obj.PreserveUnknownFields = utilpointer.BoolPtr(true)
 	}
 }
 
