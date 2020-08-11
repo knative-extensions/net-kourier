08a8f4daf5e387db24cb14b7146bc6f937afc5c6
Nghia Tran <nghia@google.com>
2020-08-11 11:10:17 -0700

    Update hack/tools.go missed in #140

--- hack/tools.go
+++ hack/tools.go
@@ -8,12 +8,12 @@ import (
 	// For chaos testing the leaderelection stuff.
 	_ "knative.dev/pkg/leaderelection/chaosduck"
 
-	_ "knative.dev/serving/test/conformance/ingress"
-	_ "knative.dev/serving/test/test_images/flaky"
-	_ "knative.dev/serving/test/test_images/grpc-ping"
-	_ "knative.dev/serving/test/test_images/helloworld"
-	_ "knative.dev/serving/test/test_images/httpproxy"
-	_ "knative.dev/serving/test/test_images/runtime"
-	_ "knative.dev/serving/test/test_images/timeout"
-	_ "knative.dev/serving/test/test_images/wsserver"
+	_ "knative.dev/networking/test/conformance/ingress"
+	_ "knative.dev/networking/test/test_images/flaky"
+	_ "knative.dev/networking/test/test_images/grpc-ping"
+	_ "knative.dev/networking/test/test_images/helloworld"
+	_ "knative.dev/networking/test/test_images/httpproxy"
+	_ "knative.dev/networking/test/test_images/runtime"
+	_ "knative.dev/networking/test/test_images/timeout"
+	_ "knative.dev/networking/test/test_images/wsserver"
 )
