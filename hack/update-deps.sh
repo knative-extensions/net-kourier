8cdd24c300ac6ad802def9e04ed8ed4e185931a8
Nghia Tran <tcnghia@gmail.com>
2020-10-12 09:27:19 -0700

    leverage test-infra go_update_deps

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -14,47 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-readonly ROOT_DIR=$(dirname $0)/..
-source ${ROOT_DIR}/vendor/knative.dev/test-infra/scripts/library.sh
-
 set -o errexit
 set -o nounset
 set -o pipefail
 
-cd ${ROOT_DIR}
+source $(dirname "$0")/../vendor/knative.dev/test-infra/scripts/library.sh
 
-# We need these flags for things to work properly.
-export GO111MODULE=on
-
-# This controls the release branch we track.
-VERSION="master"
-
-# The list of dependencies that we track at HEAD and periodically
-# float forward in this repository.
-FLOATING_DEPS=(
-  "knative.dev/networking@${VERSION}"
-  "knative.dev/test-infra@${VERSION}"
-  "knative.dev/pkg@${VERSION}"
-  "knative.dev/serving@${VERSION}"
-)
-
-# Parse flags to determine any we should pass to dep.
-GO_GET=0
-while [[ $# -ne 0 ]]; do
-  parameter=$1
-  case ${parameter} in
-    --upgrade) GO_GET=1 ;;
-    *) abort "unknown option ${parameter}" ;;
-  esac
-  shift
-done
-readonly GO_GET
-
-if (( GO_GET )); then
-  go get -d ${FLOATING_DEPS[@]}
-fi
-
-
-# Prune modules.
-go mod tidy
-go mod vendor
+go_update_deps "$@"
