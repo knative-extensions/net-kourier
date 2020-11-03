e86919010b5937b80f24b087c407b826b54c52f0
Markus Thömmes <markusthoemmes@me.com>
2020-11-03 08:49:36 +0100

    Fix the 'test-integration' target of the Makefile to successfully run tests locally

--- utils/setup.sh
+++ utils/setup.sh
@@ -14,11 +14,13 @@ echo "Deploying Knative Serving"
 KNATIVE_VERSION=v0.18.0
 kubectl apply -f https://github.com/knative/serving/releases/download/${KNATIVE_VERSION}/serving-crds.yaml
 kubectl apply -f https://github.com/knative/serving/releases/download/${KNATIVE_VERSION}/serving-core.yaml
-kubectl patch configmap/config-domain -n ${KNATIVE_NAMESPACE} --type merge -p '{"data":{"127.0.0.1.nip.io":""}}'
 kubectl patch configmap/config-network -n ${KNATIVE_NAMESPACE} --type merge -p '{"data":{"ingress.class":"kourier.ingress.networking.knative.dev"}}'
 
 echo "Deploying Kourier"
-KO_DOCKER_REPO=kind.local ko apply -Rf deploy/kourier-knative.yaml
+export KO_DOCKER_REPO
+ko resolve -f test/config -f deploy/kourier-knative.yaml | \
+  sed 's/LoadBalancer/NodePort/g' | \
+  kubectl apply -f -
 
 echo "Wait for all deployments to be up"
 for d in $(kubectl -n ${KOURIER_CONTROL_NAMESPACE} get deploy -oname)
@@ -30,6 +32,3 @@ for d in $(kubectl -n ${KOURIER_GATEWAY_NAMESPACE} get deploy -oname)
 do
   kubectl -n "${KOURIER_GATEWAY_NAMESPACE}" wait --timeout=300s --for=condition=Available "$d"
 done
-
-# shellcheck disable=SC2046
-kubectl port-forward --namespace ${KOURIER_GATEWAY_NAMESPACE} "$(kubectl get pod -n ${KOURIER_GATEWAY_NAMESPACE} -l "app=3scale-kourier-gateway" --output=jsonpath="{.items[0].metadata.name}")" 8080:8080 8081:8081 19000:19000 8443:8443 &>/dev/null &
