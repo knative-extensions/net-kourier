0a9ff1c02638e2b7d7c153f2fa3c8dfc6d0b4596
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2020-11-18 12:05:25 +0900

    Run hack/update-k8s-deps.sh 0.18.12

--- hack/update-k8s-deps.sh
+++ hack/update-k8s-deps.sh
@@ -18,32 +18,11 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-export GO111MODULE=on
-export K8S_VERSION="${1:-v0.18.8}"
+source $(dirname "$0")/../vendor/knative.dev/hack/library.sh
 
-K8S_DEPS=(
-  "k8s.io/api"
-  "k8s.io/apiextensions-apiserver"
-  "k8s.io/apimachinery"
-  "k8s.io/code-generator"
-  "k8s.io/client-go"
-)
-
-function update_module {
-  local dep="${1}"
-  local version="${2}"
-
-
-  echo "Updating ${dep} to ${version}"
-
-  go mod edit \
-    -require="${dep}@${version}" \
-    -replace="${dep}=${dep}@${version}"
-}
-
-for dep in "${K8S_DEPS[@]}"
-do
-  update_module "${dep}" "${K8S_VERSION}"
-done
+run_go_tool knative.dev/test-infra/buoy \
+  buoy float ${REPO_ROOT_DIR}/go.mod \
+    --release "$1" --domain k8s.io --ruleset=Release \
+    | xargs -n1 -t go get -d
 
 ./hack/update-deps.sh
