44c4cd3efd724acd22ee69c22939df077b22fe6c
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2021-02-08 13:30:56 +0900

    Add retry test image
    This patch adds retry test image which was added by
    https://github.com/knative/networking/commit/45b7ed13aeaa6276d6399cec0e3354c8acd8dbd0.

--- hack/tools.go
+++ hack/tools.go
@@ -12,6 +12,7 @@ import (
 	_ "knative.dev/networking/test/conformance/ingress"
 	_ "knative.dev/networking/test/test_images/grpc-ping"
 	_ "knative.dev/networking/test/test_images/httpproxy"
+	_ "knative.dev/networking/test/test_images/retry"
 	_ "knative.dev/networking/test/test_images/runtime"
 	_ "knative.dev/networking/test/test_images/timeout"
 	_ "knative.dev/networking/test/test_images/wsserver"
