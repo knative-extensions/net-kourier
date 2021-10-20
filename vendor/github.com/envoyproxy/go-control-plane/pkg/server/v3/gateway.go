dbeafd769e4887a5ae0524a8792240006df970c1
Markus Thömmes <markusthoemmes@me.com>
2021-10-20 09:50:22 +0200

    Bump assorted dependencies

--- vendor/github.com/envoyproxy/go-control-plane/pkg/server/v3/gateway.go
+++ vendor/github.com/envoyproxy/go-control-plane/pkg/server/v3/gateway.go
@@ -25,16 +25,12 @@ import (
 
 	discovery "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
 	"github.com/envoyproxy/go-control-plane/pkg/cache/types"
-	"github.com/envoyproxy/go-control-plane/pkg/log"
 	"github.com/envoyproxy/go-control-plane/pkg/resource/v3"
 )
 
 // HTTPGateway is a custom implementation of [gRPC gateway](https://github.com/grpc-ecosystem/grpc-gateway)
 // specialized to Envoy xDS API.
 type HTTPGateway struct {
-	// Log is an optional log for errors in response write
-	Log log.Logger
-
 	// Server is the underlying gRPC server
 	Server Server
 }
@@ -52,6 +48,8 @@ func (h *HTTPGateway) ServeHTTP(req *http.Request) ([]byte, int, error) {
 		typeURL = resource.ListenerType
 	case resource.FetchRoutes:
 		typeURL = resource.RouteType
+	case resource.FetchScopedRoutes:
+		typeURL = resource.ScopedRouteType
 	case resource.FetchSecrets:
 		typeURL = resource.SecretType
 	case resource.FetchRuntimes:
