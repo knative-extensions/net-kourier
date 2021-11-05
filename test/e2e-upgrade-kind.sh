daf3f249ce65454acf14b5d50cd3e497410d12c4
dprotaso <dprotaso@gmail.com>
2021-11-05 15:28:59 -0400

    print target version

--- test/e2e-upgrade-kind.sh
+++ test/e2e-upgrade-kind.sh
@@ -18,58 +18,62 @@
 
 set -euo pipefail
 
-KOURIER_GATEWAY_NAMESPACE=kourier-system
-KOURIER_CONTROL_NAMESPACE=knative-serving
-TEST_NAMESPACE=serving-tests
+source $(dirname $0)/e2e-common.sh
+echo $(last_version)
+# KOURIER_GATEWAY_NAMESPACE=kourier-system
+# KOURIER_CONTROL_NAMESPACE=knative-serving
+# TEST_NAMESPACE=serving-tests
 
-export KO_DOCKER_REPO=kind.local
-export KIND_CLUSTER_NAME="kourier-integration"
-$(dirname $0)/upload-test-images.sh
+# echo $(!)
 
-echo ">> Setup test resources"
-ko apply -f test/config
+# # export KO_DOCKER_REPO=kind.local
+# # export KIND_CLUSTER_NAME="kourier-integration"
+# # $(dirname $0)/upload-test-images.sh
 
-# Exclude the control-plane node, which doesn't seem to expose the nodeport service.
-IPS=( $(kubectl get nodes -lkubernetes.io/hostname!=kind-control-plane -ojsonpath='{.items[*].status.addresses[?(@.type=="InternalIP")].address}') )
+# # echo ">> Setup test resources"
+# # ko apply -f test/config
 
-export "GATEWAY_OVERRIDE=kourier"
-export "GATEWAY_NAMESPACE_OVERRIDE=${KOURIER_GATEWAY_NAMESPACE}"
+# # # Exclude the control-plane node, which doesn't seem to expose the nodeport service.
+# # IPS=( $(kubectl get nodes -lkubernetes.io/hostname!=kind-control-plane -ojsonpath='{.items[*].status.addresses[?(@.type=="InternalIP")].address}') )
 
-echo "Install the old Kourier version"
-kubectl apply -f "https://github.com/knative-sandbox/net-kourier/releases/download/v0.19.1/release.yaml"
+# # export "GATEWAY_OVERRIDE=kourier"
+# # export "GATEWAY_NAMESPACE_OVERRIDE=${KOURIER_GATEWAY_NAMESPACE}"
 
-echo "Wait for all deployments to be up"
-kubectl -n "${KOURIER_CONTROL_NAMESPACE}" wait --timeout=300s --for=condition=Available deployment/3scale-kourier-control
-kubectl -n "${KOURIER_GATEWAY_NAMESPACE}" wait --timeout=300s --for=condition=Available deployment/3scale-kourier-gateway
+# # echo "Install the old Kourier version"
+# # kubectl apply -f "https://github.com/knative-sandbox/net-kourier/releases/download/v0.19.1/release.yaml"
 
-# Remove the following files in case we failed to clean them up in an earlier test.
-rm -f /tmp/prober-signal
+# # echo "Wait for all deployments to be up"
+# # kubectl -n "${KOURIER_CONTROL_NAMESPACE}" wait --timeout=300s --for=condition=Available deployment/3scale-kourier-control
+# # kubectl -n "${KOURIER_GATEWAY_NAMESPACE}" wait --timeout=300s --for=condition=Available deployment/3scale-kourier-gateway
 
-echo "Running prober test"
-go test -count=1 -timeout=20m -tags=probe ./test/upgrade/... \
-  --ingressendpoint="${IPS[0]}" \
-  --ingressClass=kourier.ingress.networking.knative.dev &
-PROBER_PID=$!
-echo "Prober PID is ${PROBER_PID}"
+# # # Remove the following files in case we failed to clean them up in an earlier test.
+# # rm -f /tmp/prober-signal
 
-# Wait for the ingress to become ready
-until [[ $(kubectl -n "${TEST_NAMESPACE}" get ingresses.networking.internal.knative.dev -oname | wc -l) == 1 ]]; do sleep 1; done
-kubectl -n "${TEST_NAMESPACE}" wait --timeout=300s --for=condition=Ready ingresses.networking.internal.knative.dev --all
+# # echo "Running prober test"
+# # go test -count=1 -timeout=20m -tags=probe ./test/upgrade/... \
+# #   --ingressendpoint="${IPS[0]}" \
+# #   --ingressClass=kourier.ingress.networking.knative.dev &
+# # PROBER_PID=$!
+# # echo "Prober PID is ${PROBER_PID}"
 
-echo "Install the current Kourier version"
-export KO_DOCKER_REPO=kind.local
-ko resolve -f config | \
-  sed 's/LoadBalancer/NodePort/g' | \
-  kubectl apply -f -
+# # # Wait for the ingress to become ready
+# # until [[ $(kubectl -n "${TEST_NAMESPACE}" get ingresses.networking.internal.knative.dev -oname | wc -l) == 1 ]]; do sleep 1; done
+# # kubectl -n "${TEST_NAMESPACE}" wait --timeout=300s --for=condition=Ready ingresses.networking.internal.knative.dev --all
 
-echo "Wait for the deployments to roll over"
-kubectl -n "${KOURIER_CONTROL_NAMESPACE}" rollout status deployment/3scale-kourier-control
-kubectl -n "${KOURIER_GATEWAY_NAMESPACE}" rollout status deployment/3scale-kourier-gateway
+# # echo "Install the current Kourier version"
+# # export KO_DOCKER_REPO=kind.local
+# # ko resolve -f config | \
+# #   sed 's/LoadBalancer/NodePort/g' | \
+# #   kubectl apply -f -
 
-echo "Wait for some more traffic to flow"
-sleep 10
+# # echo "Wait for the deployments to roll over"
+# # kubectl -n "${KOURIER_CONTROL_NAMESPACE}" rollout status deployment/3scale-kourier-control
+# # kubectl -n "${KOURIER_GATEWAY_NAMESPACE}" rollout status deployment/3scale-kourier-gateway
 
-# The probe tests are blocking on the following files to know when it should exit.
-echo "done" > /tmp/prober-signal
+# # echo "Wait for some more traffic to flow"
+# # sleep 10
 
-wait ${PROBER_PID}
+# # # The probe tests are blocking on the following files to know when it should exit.
+# # echo "done" > /tmp/prober-signal
+
+# # wait ${PROBER_PID}
