bcfcaaa4425a117cd9644c0f2afe41b8c0ab7336
knative-automation <automation@knative.team>
2022-07-28 23:37:37 -0400

    upgrade to latest dependencies (#885)
    bumping knative.dev/networking 8baa0ac...19a4157:
      > 19a4157 remove defunct markdown linting variable (# 696)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- vendor/knative.dev/networking/test/presubmit-tests.sh
+++ vendor/knative.dev/networking/test/presubmit-tests.sh
@@ -21,10 +21,6 @@
 # Use the flags --build-tests, --unit-tests and --integration-tests
 # to run a specific set of tests.
 
-# Markdown linting failures don't show up properly in Gubernator resulting
-# in a net-negative contributor experience.
-export DISABLE_MD_LINTING=1
-
 source $(dirname $0)/../vendor/knative.dev/hack/presubmit-tests.sh
 
 # TODO(mattmoor): integration tests
