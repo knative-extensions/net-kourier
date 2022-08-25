1ab21317e97752d03ae028b3743f38ef25638631
Sidharth Ramesh <tornadoalert@gmail.com>
2022-08-25 12:12:19 +0530

    Made TLS 1.2 the minimum protocol version (#905)
    * made TLS 1.2 the minimum protocol version
    
    * added comment for TLS minimum version fix

--- pkg/envoy/api/listener.go
+++ pkg/envoy/api/listener.go
@@ -217,6 +217,10 @@ func createTLSContext(certificate []byte, privateKey []byte) *auth.DownstreamTls
 	return &auth.DownstreamTlsContext{
 		CommonTlsContext: &auth.CommonTlsContext{
 			AlpnProtocols: []string{"h2", "http/1.1"},
+			// Temporary fix until we start using envoyproxy image newer than v1.23.0 (envoyproxy has adopted TLS v1.2 as the default minimum version in https://github.com/envoyproxy/envoy/commit/f8baa480ec9c6cbaa7a9d5433102efb04145cfc8)
+			TlsParams: &auth.TlsParameters{
+				TlsMinimumProtocolVersion: auth.TlsParameters_TLSv1_2,
+			},
 			TlsCertificates: []*auth.TlsCertificate{{
 				CertificateChain: &core.DataSource{
 					Specifier: &core.DataSource_InlineBytes{InlineBytes: certificate},
