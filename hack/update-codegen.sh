0317e46e44336cbde0a7bf1681197aaccb2b9655
ystkfujii <ystk.fujii0731@gmail.com>
2025-02-13 15:45:43 +0000

    fix: update-codegen.sh

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -19,20 +19,13 @@ set -o nounset
 set -o pipefail
 
 source $(dirname $0)/../vendor/knative.dev/hack/codegen-library.sh
+source "${CODEGEN_PKG}/kube_codegen.sh"
 
 group "Deepcopy Gen"
 
-# Broken out of code-generator scripts, since we don't need generate-groups.sh
-(
-    cd "${CODEGEN_PKG}"
-    go install ./cmd/deepcopy-gen
-)
-
-${GOPATH}/bin/deepcopy-gen \
-  -O zz_generated.deepcopy \
-  --go-header-file "${REPO_ROOT_DIR}/hack/boilerplate/boilerplate.go.txt" \
-  -i knative.dev/net-kourier/pkg/config \
-  -i knative.dev/net-kourier/pkg/reconciler/ingress/config
+kube::codegen::gen_helpers \
+  --boilerplate "${REPO_ROOT_DIR}/hack/boilerplate/boilerplate.go.txt" \
+  "${REPO_ROOT_DIR}/pkg"
 
 # Make sure our dependencies are up-to-date
 ${REPO_ROOT_DIR}/hack/update-deps.sh
